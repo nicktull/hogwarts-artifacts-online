8494ca37755ec4349c38d05a99cc8e0f0c0e7190
Nicholas Tullbane <n.tullbane@tcu.edu>
2024-03-06 00:01:17 -0600

    Artifact CRUD done! Close #1

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactNotFoundException.java
@@ -0,0 +1,10 @@
+package edu.tcu.cs.hogwartsartifactsonline.artifact;
+
+public class ArtifactNotFoundException extends RuntimeException{
+
+    public ArtifactNotFoundException(String id){
+        super("Could not find artifact with Id " + id + " :(");
+    }
+
+
+}
